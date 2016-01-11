97840e100b09782d927ffa5ce5c5a9b25abf2204
Bruno Barbosa <bsbruno1@gmail.com>
2016-01-11 01:55:09 -0200

    add theme app

--- estavos/settings.py
+++ estavos/settings.py
@@ -196,6 +196,7 @@ TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, "templates"),)
 ################
 
 INSTALLED_APPS = (
+    'estavos.theme',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
