4e065b6a5085132c2e17861634c6ddc3d3b07131
Bruno Barbosa <bsbruno1@gmail.com>
2016-02-25 00:09:30 -0300

    add merge migration

---
+++ estavos/courses/migrations/0011_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('courses', '0010_auto_20160224_1731'),
+        ('courses', '0010_auto_20160223_2335'),
+    ]
+
+    operations = [
+    ]
