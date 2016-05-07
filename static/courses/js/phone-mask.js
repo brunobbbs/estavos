8e73bef0357e5e8bbf9aae54ea9205abb15a3131
Bruno Barbosa <bsbruno1@gmail.com>
2016-05-07 14:34:51 -0300

    add some staticfiles

---
+++ static/courses/js/phone-mask.js
@@ -0,0 +1,8 @@
+var PhoneMaskBehavior = function (val) {
+  return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+},
+phoneOptions = {
+  onKeyPress: function(val, e, field, options) {
+      field.mask(PhoneMaskBehavior.apply({}, arguments), options);
+    }
+};
