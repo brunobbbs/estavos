4fd5d5a477da16d29107954781191f2b93e8e155
Bruno Barbosa <bsbruno1@gmail.com>
2016-05-27 10:40:53 -0300

    add tournament model

--- estavos/tournaments/models.py
+++ estavos/tournaments/models.py
@@ -1,3 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
+
+class Tournament(models.Model):
+    title = models.CharField('Nome do evento', max_length=100)
+    start_date = models.DateField('Data de início')
+    end_date = models.DateField('Data de término')
+    inscriptions_date_limit = models.DateTimeField('Data limite para inscrições')
+    active = models.BooleanField('Ativo?')
+    place = models.CharField('Local', max_length=50)
+    url = models.URLField('Link da página do evento')
+
+    def __str__(self):
+        return self.title
---
+++ estavos/tournaments/tests/test_model_tournament.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from datetime import datetime, date
+
+from django.test import TestCase
+from estavos.tournaments.models import Tournament
+
+
+class TournamentModelTest(TestCase):
+
+    def setUp(self):
+        self.tournament = Tournament.objects.create(
+            title='IRT Brasiliense de Xadrez Amador 2016',
+            start_date=date(2016, 06, 17),
+            end_date=date(2016, 06, 19),
+            inscriptions_date_limit=datetime(2016, 06, 16, 20, 30, 00),
+            active=True,
+            place='Venâncio Shopping',
+            url='http://estavos.com/torneios/irt-brasiliense-de-xadrez-amador-2016-sub-2200/'
+        )
+
+    def test_create(self):
+        self.assertTrue(Tournament.objects.exists())
+
+    def test_str(self):
+        self.assertEqual('IRT Brasiliense de Xadrez Amador 2016', str(self.tournament))
