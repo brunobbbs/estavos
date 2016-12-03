4f4ac66ffd077a05095e8396c9a2ccf0e56b1c90
Bruno Barbosa <bsbruno1@gmail.com>
2016-12-03 16:58:37 -0200

    optimize imports and refator tests

--- estavos/courses/tests/test_model_course.py
+++ estavos/courses/tests/test_model_course.py
@@ -1,32 +1,31 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from datetime import date
 from decimal import Decimal
 
-from django.shortcuts import resolve_url as r
-from datetime import date
-
 from django.test import TestCase
+
 from estavos.courses.models import Course
 
 
 class CourseModelTest(TestCase):
     def setUp(self):
-        self.obj = Course(
+        self.obj = Course.objects.create(
             name='Curso APRENDA #3',
             place='Kumon Águas Claras - Av. das Castanheiras',
-            start_date=date(2016, 03, 12),
+            start_date=date(2018, 01, 21),
             classes='Crianças',
             price=Decimal('449.70'),
             is_active=True
         )
-        self.obj.save()
 
     def test_create(self):
         self.assertTrue(Course.objects.exists())
 
     def test_str(self):
-        self.assertEqual('Curso APRENDA #3 - 12/03/2016', str(self.obj))
+        self.assertEqual('Curso APRENDA #3 - 21/01/2018', str(self.obj))
 
     def test_start_date(self):
         self.assertIsInstance(self.obj.start_date, date)
+
