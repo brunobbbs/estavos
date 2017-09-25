d9dc5af07d75488bb4baa7e8a643504ff2bf4404
Bruno Barbosa <bruno.barbosa@corp.globo.com>
2017-09-25 13:18:14 -0300

    tournaments/urls: remove unnecessary slash at url pattern

--- estavos/tournaments/urls.py
+++ estavos/tournaments/urls.py
@@ -9,9 +9,9 @@ from estavos.tournaments.views import TournamentListView, InscriptionCreate
 urlpatterns = [
     url(r'^$', TournamentListView.as_view(), name='list'),
     url(r'^(?P<tournament>[\d]+)', include([
-        url(r'^/inscricoes/nova/$', InscriptionCreate.as_view(), name='inscription_new'),
-        url(r'^/inscricoes/(?P<slug>[a-f0-9]+)/$', InscriptionDetail.as_view(), name='inscription_detail'),
-        url(r'^/controle/inscricoes/$', InscriptionControlView.as_view(), name='inscriptions_control_list'),
-        url(r'^/controle/inscricoes/confirmar/(?P<pk>[\d]+)/$', InscriptionControlConfirmView.as_view(), name='inscriptions_control_confirm'),
+        url(r'^inscricoes/nova/$', InscriptionCreate.as_view(), name='inscription_new'),
+        url(r'^inscricoes/(?P<slug>[a-f0-9]+)/$', InscriptionDetail.as_view(), name='inscription_detail'),
+        url(r'^controle/inscricoes/$', InscriptionControlView.as_view(), name='inscriptions_control_list'),
+        url(r'^controle/inscricoes/confirmar/(?P<pk>[\d]+)/$', InscriptionControlConfirmView.as_view(), name='inscriptions_control_confirm'),
     ])),
 ]
