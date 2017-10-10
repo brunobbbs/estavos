5ab31a9f215ef0f403b18e98eec3b7c14c90e1ea
Bruno Barbosa <bsbruno1@gmail.com>
2017-10-09 23:51:49 -0300

    tournaments/urls: remove `TournamentDetail` route

--- estavos/tournaments/urls.py
+++ estavos/tournaments/urls.py
@@ -8,8 +8,6 @@ from estavos.tournaments.views import TournamentListView, InscriptionCreate
 
 urlpatterns = [
     url(r'^$', TournamentListView.as_view(), name='list'),
-    # url(r'^(?P<pk>[\d]+)/$', TournamentDetail.as_view(), name='detail'),
-    url(r'^(?P<slug>[-\w]+)/$', TournamentDetail.as_view(), name='detail'),
     url(r'^(?P<tournament>[\d]+)', include([
         url(r'^/inscricoes/nova/$', InscriptionCreate.as_view(), name='inscription_new'),
         url(r'^/inscricoes/(?P<slug>[a-f0-9]+)/$', InscriptionDetail.as_view(), name='inscription_detail'),
