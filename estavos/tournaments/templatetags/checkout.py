bde533a7cbcb41155a020d8090acb7eef7dc2a2a
Bruno Barbosa <bsbruno1@gmail.com>
2017-10-22 23:17:18 -0200

    tournaments/templatetags: remove `checkout`

--- estavos/tournaments/templatetags/checkout.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter('total')
-def total(inscription):
-    players = inscription.competitors.all().count()
-    price = inscription.tournament.price
-    return players * price
