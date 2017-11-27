e55265c222ada236cf0e401e54f90f4019455c3e
Bruno Barbosa <bsbruno1@gmail.com>
2017-11-27 21:03:01 -0200

    tournaments/admin: add missing models on admin

--- estavos/tournaments/admin.py
+++ estavos/tournaments/admin.py
@@ -1,18 +1,34 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.contrib import admin
-from estavos.tournaments.models import Inscription, Tournament
+from estavos.tournaments.models import Inscription, Tournament, Payment, Competitor
 from import_export.admin import ImportExportModelAdmin
 
 
-class InscriptionInline(admin.TabularInline):
+class InscriptionInline(admin.StackedInline):
     model = Inscription
+
+
+class CompetitorInline(admin.StackedInline):
+    model = Competitor
+
+
+class PaymentAdmin(ImportExportModelAdmin):
+    list_display = ('payment_type', 'status', 'paid', 'receipt')
+    list_filter = ('paid', 'status', 'payment_type')
+    inlines = [InscriptionInline]
+
+
+class CompetitorAdmin(ImportExportModelAdmin):
+    list_display = ('name', 'birth', 'id_lbx', 'id_fide')
+    list_filter = ('id_lbx', 'id_fide')
 
 
 class InscriptionAdmin(ImportExportModelAdmin):
     list_display = ('name', 'email', 'phone', 'confirmed')
     search_fields = ('name', 'email')
     list_filter = ('confirmed', 'tournament')
+    inlines = [CompetitorInline]
 
 
 class TournamentAdmin(admin.ModelAdmin):
@@ -24,4 +40,6 @@ class TournamentAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Inscription, InscriptionAdmin)
+admin.site.register(Payment, PaymentAdmin)
+admin.site.register(Competitor, CompetitorAdmin)
 admin.site.register(Tournament, TournamentAdmin)
